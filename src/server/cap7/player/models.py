3477293fcc0c3610005b8d967d7e6b1884b01e4e
JeongChanghoe <20143102@kookmin.ac.kr>
2019-04-08 11:56:43 +0900

    웹 페이지 동영상 업로드 기능 수정

--- src/server/cap7/player/models.py
+++ src/server/cap7/player/models.py
@@ -1,9 +1,20 @@
 #-*- coding: utf-8 -*-
 from django.db import models
+from django.conf import settings
 
+# 동영상 이름 랜덤으로 생성함
+def user_path(instance, filename): #파라미터 instance는 Photo 모델을 의미 filename은 업로드 된 파일의 파일 이름
+    from random import choice
+    import string # string.ascii_letters : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+    arr = [choice(string.ascii_letters) for _ in range(8)]
+    pid = ''.join(arr) # 8자리 임의의 문자를 만들어 파일명으로 지정
+    extension = filename.split('.')[-1] # 배열로 만들어 마지막 요소를 추출하여 파일확장자로 지정
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<random>
+    return '%s.%s' % (pid, extension) # 예 : wayhome/abcdefgs.png
+
+
+# 실제로 DB에 저장되는 기능
 class Video(models.Model):
-    name= models.CharField(max_length=500)
-    videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
-
+    videofile= models.FileField(upload_to= user_path, null=True, verbose_name="")
     def __str__(self):
-        return self.name + ": " + str(self.videofile)
+        return str(self.videofile)
