ce7dece40f869147f4c18bbd10217dcc2f574477
JeongChanghoe <20143102@kookmin.ac.kr>
2019-04-17 02:47:11 +0900

    기능 연결 작업 및 자막 버그 수정

--- src/server/cap7/player/generateSubtitleFormTxt.py
+++ src/server/cap7/player/generateSubtitleFormTxt.py
@@ -1,71 +1,89 @@
 
-class generateSubtitleFormTxt:
+def matchTImecode(sentence, start_time, end_time, word):
+    start, end, sen, j , check = [], [], [], 0, 0
+
+    for i in range(0, len(sentence)):
+        sentence[i] = sentence[i].strip()
+        sen.append(sentence[i].strip('?'))
+
+    for i in range(0,len(sentence)):
+        while j < len(word):
+            if(sen[i].startswith(word[j]) and check == 0):
+                start.append(start_time[j])
+                check = 1
+            if(sen[i].endswith(word[j]) and check == 1):
+                end.append(end_time[j])
+                check = 0
+                j+=1
+                break
+            j+=1
+
+    return start, end
+
+def convertTimecode(stt_t):
+    srt_t = "00:00:00.000"
+
+    # find_dot = stt_t.find('.')
+    # if find_dot == -1:
+    #     stt_t = stt_t[0:len(stt_t)-1] + ".000" +stt_t[-1]
+
+    front_t = int(stt_t)
+    back_t = int(round(stt_t -int(stt_t), 3)*1000)
+    if(back_t==0):
+        back_t = "000"
+
+    h1 = int(front_t / 36000)
+    h2 = int((front_t / 3600) % 10)
+    m1 = int(front_t / 600)
+    m2 = int((front_t / 60) % 10)
+    s1 = int((front_t % 60) / 10)
+    s2 = int((front_t % 60) % 10)
+
+    srt_t = str(h1) + str(h2) + ":" + str(m1) + str(m2) + ":" + str(s1) + str(s2) + "." + str(back_t)
+
+    return srt_t
+
+def writeSubtitle(sentence, start_time, end_time, file_name):
+    num, arrow = 0, " --> "
+    f = open(file_name, 'w')
+    f.write("WEBVTT\n\n")
+
+    for x in range(0, len(sentence)):
+        num += 1
+        f.write(str(num))
+        f.write("\n")
+        f.write(start_time[x])
+        f.write(arrow)
+        f.write(end_time[x])
+        f.write("\n")
+        f.write(sentence[x])
+        f.write("\n\n")
+    f.close()
+
+def sliceLongSentence(sentence):
+    i, sen_len = 0, len(sentence)
+    while (i < sen_len):
+        count_blank = sentence[i].count(' ')
+        if (count_blank > 12):
+            count, find_index = 0, 0
+            sent = sentence[i]
+            for j in range(len(sent)):
+                find_index = sent.index(' ', find_index + 1)
+                count += 1
+                if (count == 6):
+                    sentence[i:i + 1] = [sent[0:find_index], sent[find_index + 1:]]
+                    sen_len += 1
+                    break
+        i += 1
+    return sentence
+
+def generateSubtitle(input_path, output_path, file_name):
     transcripts, sentence, start_time, end_time, word = "", [], [], [], []
     sent_start, sent_end = [], []
-
-    def matchTImecode(sentence, start_time, end_time, word):
-        start, end, sen, j , check = [], [], [], 0, 0
-
-        for i in range(0, len(sentence)):
-            sentence[i] = sentence[i].strip()
-            sen.append(sentence[i].strip('?'))
-
-        for i in range(0,len(sentence)):
-            while j < len(word):
-                if(sen[i].startswith(word[j]) and check == 0):
-                    start.append(start_time[j])
-                    check = 1
-                if(sen[i].endswith(word[j]) and check == 1):
-                    end.append(end_time[j])
-                    check = 0
-                    j+=1
-                    break
-                j+=1
-
-        return start, end
-
-    def convertTimecode(stt_t):
-        srt_t = "00:00:00,000"
-
-        # find_dot = stt_t.find('.')
-        # if find_dot == -1:
-        #     stt_t = stt_t[0:len(stt_t)-1] + ".000" +stt_t[-1]
-
-        front_t = int(stt_t)
-        back_t = int(round(stt_t -int(stt_t), 3)*1000)
-        if(back_t==0):
-            back_t = "000"
-
-        h1 = int(front_t / 36000)
-        h2 = int((front_t / 3600) % 10)
-        m1 = int(front_t / 600)
-        m2 = int((front_t / 60) % 10)
-        s1 = int((front_t % 60) / 10)
-        s2 = int((front_t % 60) % 10)
-
-        srt_t = str(h1) + str(h2) + ":" + str(m1) + str(m2) + ":" + str(s1) + str(s2) + "," + str(back_t)
-
-        return srt_t
-
-    def writeSubtitle(sentence, start_time, end_time, file_name):
-        num, arrow = 0, " --> "
-
-        srt_path = 'player/media/videos/test.srt'
-        f = open(srt_path, 'w')
-        for x in range(0, len(sentence)):
-            num += 1
-            f.write(str(num))
-            f.write("\n")
-            f.write(start_time[x])
-            f.write(arrow)
-            f.write(end_time[x])
-            f.write("\n")
-            f.write(sentence[x])
-            f.write("\n\n")
-        f.close()
+    output_file = output_path+file_name+'.vtt'
 
     # Load data file
-    input_file = open('player/media/text/test.txt', 'r', encoding='utf-8')
+    input_file = open(input_path, 'r', encoding='utf-8')
     lines = input_file.readlines()
     for line in lines:
         if (line.startswith('Transcript:')):
@@ -79,19 +97,30 @@ class generateSubtitleFormTxt:
         end_time.append(round(float(item[4]),1))
 
     # Divide the transcript into sentence units
+    if(transcripts.endswith('.')):
+        transcripts = transcripts.rstrip('.')
     transcripts = transcripts.replace('?', '?.')
     transcripts = transcripts.replace('!', '!.')
     transcripts = transcripts.replace(',', '.')
     sentence = transcripts.split('.')
 
+    # Slice a long sentence
+    sentence = sliceLongSentence(sentence)
+
     # Match the time code of each sentence
     sent_start, sent_end = matchTImecode(sentence, start_time, end_time, word)
 
-    # Convert the time code to SRT format
+    # Convert the time code to VTT format
     for x in range(0, len(sent_start)):
         sent_start[x] = convertTimecode(sent_start[x])
 
     for x in range(0, len(sent_end)):
         sent_end[x] = convertTimecode(sent_end[x])
 
-    writeSubtitle(sentence, sent_start, sent_end, '0412.srt')
+    writeSubtitle(sentence, sent_start, sent_end, output_file)
+
+if __name__=='__main__':
+    input_path = 'textfile_2.txt'
+    output_path = ''
+    output_name = '180415'
+    generateSubtitle(input_path, output_path, output_name)
--- src/server/cap7/player/views.py
+++ src/server/cap7/player/views.py
@@ -1,68 +1,45 @@
 #-*- coding: utf-8 -*-
 
-import json
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
-from django.utils import timezone
 from .models import Video
 from .forms import VideoForm
 from .transcribe import transcribe_file
 from .videotowav import *
 from .generateSubtitleFormTxt import *
 import os
-
-# def upload_video(request):
-#     if request.method == 'POST':
-#         form = VideoForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect('/success')
-#     else:
-#         form = VideoForm()
-#
-#     return render(request, 'player.html', {'form': form})
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
 
 
 def player(request):
-
     try:
         lastvideo= Video.objects.last()
         videofile= lastvideo.videofile
-        #subscript = transcribe_file('/player/media/videos/' + videofile)
-
+        srt_path = 'player/media/videos/'
+        video_name = str(videofile)
+        file, file_extension = os.path.splitext(video_name)
+        file = pipes.quote(file)
+        audio_path = video_to_audio(video_name)
+        text_path = transcribe_file(audio_path)
+        generateSubtitle(text_path, srt_path, 'test')
     except:
         videofile=''
-        wavfile=''
-        #subscript=''
 
     form = VideoForm(request.POST or None, request.FILES or None)
 
-    video_name = str(videofile)
-    file, file_extension = os.path.splitext(video_name)
-    file = pipes.quote(file)
-
     if form.is_valid():
         try:
-            print('@@@@@@@@@@@@')
             print(video_name)
             if file_extension != '.mp4':
                 video_to_mp4(video_name)
                 video = Video.objects.get(name=video_name)
 
                 video.videofile = settings.MEDIA_ROOT + str(file) + '.mp4'
-                print('@@@@@new_path')
                 print(videofile.video)
                 video.save()
-                print('왜안돼')
                 video_name = str(file) + '.mp4'
 
-            audio_path = video_to_audio(video_name)
-            print(audio_path)
-            text_path = transcribe_file(audio_path)
-            writeSubtitle(text_path)
-            print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-            print(text_path)
         except:
             pass
 
@@ -70,10 +47,6 @@ def player(request):
 
     context = {
                 'videofile': videofile,
-                'form': form,
-                #'subscript' : subscript
+                'form': form
               }
-    print('#############')
-    print(video_name)
-    #print(subscript)
     return render(request, 'player/player.html', context)
