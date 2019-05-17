083565fceff091e521da2ff7d43319835581f75f
배진영 <wlsdudqo97@naver.com>
2019-05-17 18:09:14 +0900

    delete same morpheme
    중복되는 형태소 제거

--- src/NLP/main.py
+++ src/NLP/main.py
@@ -10,8 +10,8 @@ def splitLine(line):
     return line
 
 def main():
-    input_f = open('txt/VTT01.vtt', 'r')
-    output_f = open('txt/jedong.vtt', 'w')
+    input_f = open('txt/VTT05.vtt', 'r')
+    output_f = open('txt/강형욱.vtt', 'w')
 
     lines = input_f.readlines()
     for i in range(2):
@@ -19,6 +19,8 @@ def main():
         del lines[0]
 
     flag = 1
+    q = 0; v = 0
+    sentence = []
     for i in range(len(lines)):
         if flag % 4 == 0:
             output_f.write('\n')
@@ -27,10 +29,18 @@ def main():
             line = komoran.get_list(lines[i])
             line = splitLine(line)
             print(line)
+            i = 0
             for w, m in line:
                 r, word = pr.process_morph(m, w)
                 if r == 1:
-                    output_f.write(word + ' ')
+                    if (word == 'ㅂ니까') or (word == '하다'):
+                        if sentence[len(sentence)-1] != word:
+                            sentence.append(word)
+                    else:
+                        sentence.append(word)
+                i += 1
+            for w in sentence: output_f.write(w + ' ')
+            sentence.clear()
             flag+=1
         else:
             output_f.write(lines[i])
@@ -43,7 +53,7 @@ def main():
 
 if __name__ == "__main__":
     komoran = Komoran(DEFAULT_MODEL['FULL'])
-    komoran.set_user_dic("dic/dic.user")
+    komoran.set_user_dic('/Users/wlsdu/Desktop/data_mining/dictionary/dic.user')
     pr = StopWord()
     main()
     pass
