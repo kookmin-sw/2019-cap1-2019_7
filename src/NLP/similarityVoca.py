117517db4b1ca70a2468993b824159a02d47f87c
배진영 <wlsdudqo97@naver.com>
2019-05-27 21:33:20 +0900

    change return value of calc_similarity function
    mapping 함수와 기능을 연결하기 위해 return 값 변경

--- src/NLP/similarityVoca.py
+++ src/NLP/similarityVoca.py
@@ -37,21 +37,22 @@ class SimilarytyWord:
             response = self.similarity_voca(true_word, ref_word[i])
             response = json.loads(str(response.data, "utf-8"))
             sim = response["return_object"]["WWN WordRelInfo"]["WordRelInfo"]["Similarity"]
-            print(str(ref_word[i]) + ": "+ str(sim))
+            # print(str(ref_word[i]) + ": "+ str(sim))
             for i in range(len(sim)):
-                result_sim.append(sim[i]["SimScore"])
                 s = + sim[i]["SimScore"]
             a = s / len(sim)
-            l = np.sort(result_sim)
-            print('평균: '+str(a))
-            print('중간값: '+str(l[5]))
-        # return result_sim.index(max(result_sim))
-        return max(result_sim)
+            result_sim.append(a)
+            # print('평균: '+str(a))
+        print(result_sim)
+        if max(result_sim) <= 0.0:
+            return -1
+        return result_sim.index(max(result_sim))
 
 if __name__ == '__main__':
     sim = SimilarytyWord()
-    input_t = '제목'
-    input_r = ['장소', '근거']
+    input_t = '공'
+    input_r = ['회사', '직업', '개인']
     a = sim.calc_similarity(input_t, input_r)
+    print(a)
     pass
 
